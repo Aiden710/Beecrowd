8119f6756f84747b5955903be3e859da73e3817c
MD. Billal <157817847+Aiden710@users.noreply.github.com>
2024-02-15 01:24:59 +0600

    Add files via upload

--- Salary with Bonus.cpp
+++ Salary with Bonus.cpp
@@ -1,5 +1,13 @@
 #include <iostream>
+#include <iomanip>
 using namespace std;
-int main(){
-    
+int main(){ 
+    string name;
+    double fixedNumver, total,consider,sunn;
+    cin >>name>>fixedNumver>>total;
+    consider=(total*0.15);
+    sunn=fixedNumver+consider;
+    cout <<fixed<<"\n"<<setprecision(2)<<"\n";
+    cout<<"TOTAL = R$ "<<sunn<<endl;
+    return 0;
 }
